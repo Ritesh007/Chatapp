a991ad64c8f2159d9609c094697f01bf68c78086
ritesh007 <rithu.ritesh@gmail.com>
2017-06-11 16:33:11 -0400

    chat app & utility scripts TODO updated +commit11

---
+++ Chatapp/Chat_consoleapp/Test/accessFiles_test.py
@@ -0,0 +1,24 @@
+"""
+Unit test case for accessFiles.py Library.
+"""
+
+import unittest
+from accessFiles import AccessFiles
+
+# TODO write a successful testcase
+class AccessFilestest(unittest.TestCase):
+
+    @staticmethod
+    def store_texts_test():
+        AccessFiles.store_texts("\n This is a test line ---> ignore this \n")
+        store_file=open("../Chatapp/Chat_consoleapp/files/chat.txt","r")
+        for i in store_file:
+            if i == "This is a test line ---> ignore this":
+              assert i,"This is a test line ---> ignore this"
+              break
+            return False
+
+def main():
+  AccessFilestest.store_texts_test()
+
+main()
---
+++ Utility_Scripts/pythonscripts/src/main/resources/Accessurl.py
@@ -0,0 +1,5 @@
+"""
+This code shows a simple way to access a Rest url that is exposed
+&
+Read the output send by that url.
+"""
