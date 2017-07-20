053d75e7aa2a42c2419547f694a9e5fdcffe62ab
ritesh007 <rithu.ritesh@gmail.com>
2017-07-19 23:12:40 -0400

    details with project structure updated

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 # Written in Python3.
 #
 ######################################################
+
+Project Structure -
+Chatapp
+   - __main__.py
+   - setup.py
+   - Chatapp
+        - __init__.py
+        - files
+            - chat.txt
+            - users.txt
+        - test
+            - __init__.py
+            - accessFiles_test.py
+        - utilities
+            - __init__.py
+            - accessFiles.py
+            - history.py
--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
 """
-This is used to build a exe file for this Application
-To run this file ---->python C:\Users\Kuchukulla\PycharmProjects\Python\Chatapp\Chat_consoleapp\setup.py build
+writes setup.py in distutils to create a zip file
 """
 
-import sys
-from cx_Freeze import setup, Executable
-
-
-exe = Executable(
-      script="C:\\Users\\Kuchukulla\\PycharmProjects\\Python\\Chatapp\\Chat_consoleapp\\main.py",
-      base="console",
-      targetName="C:\\Users\\Kuchukulla\\PycharmProjects\\Python\\Chatapp\\Chat_consoleapp\\executable\\ConsoleChatApp.exe"
-     )
-setup(
-      name="ConsoleChatApp.exe",
-      version="1.0",
-      author="Ritesh",
-      description="Console Chat Application",
-      executables=[exe]
-      )
