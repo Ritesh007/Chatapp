fa133dca8c60f213ee09380ebee3586d19e671b9
ritesh007 <rithu.ritesh@gmail.com>
2017-09-28 23:56:12 -0400

    sample testcase.

--- Chatapp/Test/accessFiles_test.py
+++ Chatapp/Test/accessFiles_test.py
@@ -3,13 +3,14 @@ Unit test case for accessFiles.py Library.
 """
 
 import unittest
-from accessFiles import AccessFiles
+#from accessFiles import AccessFiles
 
 # TODO write a successful testcase
 class AccessFilestest(unittest.TestCase):
-
-    @staticmethod
+    pass
+    """"@staticmethod
     def store_texts_test():
+        pass
         AccessFiles.store_texts("\n This is a test line ---> ignore this \n")
         store_file=open("../Chatapp/Chat_consoleapp/files/chat.txt","r")
         for i in store_file:
@@ -18,7 +19,8 @@ class AccessFilestest(unittest.TestCase):
               break
             return False
 
-def main():
-  AccessFilestest.store_texts_test()
 
-main()
+def main():
+  AccessFilestest.store_texts_test()"""
+
+#main()
